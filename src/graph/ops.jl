9666a2279cff7c41450650505cdf230682f30166
ayush-1506 <ayush.shridhar1506@gmail.com>
2019-04-25 17:22:29 +0530

    testing batchnorm

--- src/graph/ops.jl
+++ src/graph/ops.jl
@@ -32,6 +32,12 @@ ops[:Conv] = function(params, x, w, b...)
     return vcall(:conv4, w, x, Symbol("padding=$(params[:pads])"), Symbol("mode = 1"))
 end
 
+ops[:Concat] = function (params, ip1, ip2)
+  s = vcall(:ndims, ip1)
+
+  return vcall(:cat, ip1, ip2, Symbol("dims =1"))
+end
+
 ops[:Add] = function(params, A, B)
   s1 = vcall(:size, A)
   s2 = vcall(:size, B)
@@ -39,6 +45,17 @@ ops[:Add] = function(params, A, B)
     return vcall(:Add, params[:axis], A, B)
   else
     return vcall(:.+, A, B)
+  end
+end
+
+ops[:Mul] = function (params, A, B)
+  if (haskey(params, :broadcast) && params[:broadcast] == 1)
+    if !haskey(params, :axis)
+      return vcall(:.*, A, B)
+    end
+    return vcall( :Mul, params[:axis], A, B)              # To-Do define Mul function
+  else
+    return vcall(:.*, A, B)   # In case of no broadcast, Perform normal Mul operation.
   end
 end
 
@@ -50,8 +67,31 @@ ops[:MaxPool] = function(params, x)
   return vcall(:pool, x, Symbol("mode=0"), Symbol("window=$(params[:kernel_shape])"))
 end
 
+ops[:AveragePool] = function(params, x)
+  return vcall(:pool, x, Symbol("mode=1"), Symbol("window=$(params[:kernel_shape])"))
+end
+
 ops[:MatMul] = function(params, A, B)
   #tempa = vcall(:permutedims, A, vcall(:reverse, vcall(:range, 1, vcall(:ndims, A))))
   #tempb = vcall(:permutedims, B, vcall(:reverse, vcall(:range, 1, vcall(:ndims, B))))
   vcall(:*, B, A)
+end
+
+ops[:BatchNormalization] = function (params, x, scale, b, mean, var)
+  if !haskey(params ,Symbol("momentum"))
+    params[:momentum] = 0.9
+  end
+  if !haskey(params, Symbol("epsilon"))
+    params[:epsilon] = 1e-5
+  end
+  bv = vcall(:BNMoments, params[:momentum], mean, var, zeros, ones)
+  return vcall(:batchnorm, x, bv, Symbol("epsilon=$(params[:epsilon])"), Symbol("training=false"))
+end
+
+ops[:Unsqueeze] = function(params, x)
+  return vcall(:unsqueeze, x, params[:axes])
+end
+
+ops[:GlobalAveragePool] = function (params, x)
+  vcall(:mean, x, Symbol("dims = (1,2)"))
 end
